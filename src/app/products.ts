245c6e238cb5f0b7bf2467e57c228028cdd3d9e6
Sergen Bakır <cuseg09@gmail.com>
2022-01-09 00:36:52 +0300

    lesson one

---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: 1,
+    name: "Macbook",
+    imagePath: "assets/images/macbook.jpg",
+    price: 25000.45,
+    quantity: 0,
+    unit: "Adet",
+  },
+  {
+    id: 2,
+    name: "Macbook Pro X",
+    imagePath: "assets/images/macbook.jpg",
+    price: 35000.67,
+    quantity: 0,
+    unit: "Adet",
+  },
+  {
+    id: 2,
+    name: "Macbook Pro M",
+    imagePath: "assets/images/macbook.jpg",
+    price: 95000,
+    quantity: 0,
+    unit: "Adet",
+  },
+  {
+    id: 2,
+    name: "Macbook Pro SM",
+    imagePath: "assets/images/macbook.jpg",
+    price: 56000,
+    quantity: 0,
+    unit: "Adet",
+  },
+];
